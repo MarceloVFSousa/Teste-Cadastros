216a02e4eb16a8e49fbf969aab30cc7dab3cd48a
MarceloVFSousa <159039777+MarceloVFSousa@users.noreply.github.com>
2025-02-19 10:46:40 -0300

    Feature: Adicionada a tabela de visualização de produtos na página de Dashboard; Alteração da pasta dos arquivos html para suas respectivas pastas; Ajuste na classe de Produto; Ajuste na HomeController (adicionadas as variáveis para visualização da tabela na dashboard); Conserto do redirecionamento na CadastrosController; Adicionado botão de Voltar à tela CadastroSucesso; Correção da navbar no Layout principal; Criação da ProdutoViewModel

--- TesteCadastros/Controllers/HomeController.cs
+++ TesteCadastros/Controllers/HomeController.cs
@@ -1,15 +1,20 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TesteCadastros.Data;
 using TesteCadastros.Models;
+using TesteCadastros.ViewModels;
 
 namespace TesteCadastros.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly ApplicationDbContext _context;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ApplicationDbContext context, ILogger<HomeController> logger)
     {
+        _context = context;
         _logger = logger;
     }
 
@@ -23,9 +28,23 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Dashboard() 
+    public IActionResult Dashboard()
     {
-        return View();
+        var produtos = _context.Produtos
+            .Select(p => new ProdutoViewModel
+            {
+                Id = p.Id,
+                NomeProduto = p.NomeProduto,
+                Preco = p.Preco,
+                DataRegistro = p.DataRegistro,
+                DataPrevisao = p.DataPrevisao,
+                Cidade = p.Cidade,
+                Estado = p.Estado,
+                Cep = p.Cep
+            })
+            .ToList();
+
+        return View(produtos);
     }
 
     public IActionResult Cadastros() 
--- TesteCadastros/Views/Cadastros/Cadastros.cshtml.cs
+++ TesteCadastros/Views/Cadastros/Cadastros.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace TesteCadastros.Views.Home
+namespace TesteCadastros.Views.Cadastros
 {
     public class CadastrosModel : PageModel
     {
